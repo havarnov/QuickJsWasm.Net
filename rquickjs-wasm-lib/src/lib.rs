b79e8dfab26297e43d4e30de8396419507a9a448
Håvar Nøvik <havar@novik.email>
2026-04-17 15:05:48 +0200

    draft.

--- rquickjs-wasm-lib/src/lib.rs
+++ rquickjs-wasm-lib/src/lib.rs
@@ -1,11 +1,11 @@
 use rquickjs::{Context, Function, Runtime};
 
-use crate::bindings::exports::local::calculator::api::Guest;
-use crate::bindings::local::calculator::callback_handler;
+use crate::bindings::exports::rquickjs::wasm::engine_api::Guest;
+use crate::bindings::rquickjs::wasm::callbacks;
 
 #[allow(unused)]
 mod bindings {
-    wit_bindgen::generate!({ world: "calculator" });
+    wit_bindgen::generate!({ world: "rquickjs" });
 }
 
 struct Engine {
@@ -14,7 +14,7 @@ struct Engine {
 }
 
 // 1. Implement the trait for the resource inside the interface
-impl bindings::exports::local::calculator::api::GuestEngine for Engine {
+impl bindings::exports::rquickjs::wasm::engine_api::GuestEngine for Engine {
     fn new() -> Self {
         let runtime = Runtime::new().unwrap();
         let context = Context::full(&runtime).unwrap();
@@ -24,33 +24,31 @@ impl bindings::exports::local::calculator::api::GuestEngine for Engine {
         }
     }
 
-    fn add(&self, x: u32, y: u32) -> u32 {
-        self.ctx.with(|ctx| {
-            ctx.eval::<u32, _>(format!(
-                "var foo = (foo == undefined ? 0 : foo) + {x} + {y} + __print({x}, {y}); foo"
-            ))
-            .unwrap()
-        })
-    }
-
-    fn f(&self, cb: callback_handler::Callback) -> u32 {
+    fn eval(&self, script: String) {
         self.ctx
             .with(|ctx| -> Result<(), ()> {
-                let cb = cb;
+                ctx.eval::<(), _>(script).unwrap();
+                Ok(())
+            })
+            .unwrap();
+    }
+
+    fn register(&self, name: String, func: callbacks::FUnitUnit) {
+        self.ctx
+            .with(|ctx| -> Result<(), ()> {
+                let func = func;
                 let global = ctx.globals();
 
                 _ = global.set(
-                    "__print",
-                    Function::new(ctx.clone(), move || cb.get_value())
+                    &name,
+                    Function::new(ctx.clone(), move || func.get_implementation())
                         .unwrap()
-                        .with_name("__print")
+                        .with_name(&name)
                         .unwrap(),
                 );
                 Ok(())
             })
             .unwrap();
-
-        32
     }
 }
 
